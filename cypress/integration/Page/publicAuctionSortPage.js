c5a67348d4413a1c9fb23b7e032e8cdefda56605
namthtran98 <43946963+namthtran98@users.noreply.github.com>
2022-01-14 14:21:34 +0700

    Updata SortPage

--- cypress/integration/Page/publicAuctionSortPage.js
+++ cypress/integration/Page/publicAuctionSortPage.js
@@ -1,72 +1,106 @@
-export class PublicAuctionPageSort{
-    VerifyCRAsc(){
-        cy.get('[data-test-id="sorting"]').then(listArrangment => {
-            cy.wrap(listArrangment).select('conditionGradeAsc')
-            cy.get('[data-test-id="listings"]').find('[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]').then(checkMake => {
-                var arr = []
-                    cy.wrap(checkMake).each(listItem => {
-                        cy.wrap(listItem).find('[data-test-id="condition report"]').invoke('attr', 'data-value-text').then((number) => {
-                            const num = Number(number)
-                            arr.push(num)                                         
-                        })                       
-                    }).then(checkIncrease => {
-                        expect(isIncreasing(arr)).to.be.true
-                    })  
-            })
-        })
-    }
-    verifyCRDesc(){
-        cy.get('[data-test-id="sorting"]').then(listArrangment => {
-            cy.wrap(listArrangment).select('conditionGradeDesc')
-            cy.get('[data-test-id="listings"]').find('[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]').then(checkMake => {
-                var arr = []
-                    cy.wrap(checkMake).each(listItem => {
-                        cy.wrap(listItem).find('[data-test-id="condition report"]').invoke('attr', 'data-value-text').then((number) => {
-                            const num = Number(number)
-                            arr.push(num)                                         
-                        })                       
-                    }).then(checkIncrease => {
-                        expect(isIncreasing(arr)).to.be.false
-                    })  
-            })
-        })
-    }
-    verifyYearAsc(){
-        cy.get('[data-test-id="sorting"]').then(listArrangment => {
-            cy.wrap(listArrangment).select('yearAsc')
-            cy.wait(5000)
-            cy.get('[data-test-id="listings"]').find('[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]').then(check => {
-                var arr = []
-                cy.wrap(check).each(listItem => {
-                    cy.wrap(listItem).find('[data-test-id="year"]').invoke('text').then(year => {
-                        arr.push(Number(year))
-                    })
-                }).then(checkIncrease => {
-                    expect(isIncreasing(arr)).to.be.true
-                })
-            })
-        })
-    }
-    verifyYearDesc(){
-        cy.get('[data-test-id="sorting"]').then(listArrangment => {
-            cy.wrap(listArrangment).select('yearDesc')
-            cy.wait(5000)
-            cy.get('[data-test-id="listings"]').find('[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]').then(check => {
-                var arr = []
-                cy.wrap(check).each(listItem => {
-                    cy.wrap(listItem).find('[data-test-id="year"]').invoke('text').then(year => {
-                        arr.push(Number(year))
-                    })
-                }).then(checkIncrease => {
-                    expect(isIncreasing(arr)).to.be.false
-                })
-            })
-        })
-    }
+export class PublicAuctionPageSort {
+  VerifyCRAsc() {
+    cy.get('[data-test-id="sorting"]').then((listArrangment) => {
+      selectSortOotion(listArrangment, 1);
+      verifySortCRAsc();
+    });
+  }
+  verifyCRDesc() {
+    cy.get('[data-test-id="sorting"]').then((listArrangment) => {
+    selectSortOotion(listArrangment, 2);
+    verifySortCRDesc();
+    });
+  }
+  verifyYearAsc() {
+    cy.get('[data-test-id="sorting"]').then((listArrangment) => {
+    selectSortOotion(listArrangment, 14);
+    verifySortYearAsc();
+    });
+  }
+  verifyYearDesc() {
+    cy.get('[data-test-id="sorting"]').then((listArrangment) => {
+    selectSortOotion(listArrangment, 15);
+    verifySortYearDesc();
+    });
+  }
 }
-export const auctionPageSort = new PublicAuctionPageSort()
+function selectSortOotion(name, index) {
+  cy.get(name).select(index);
+  cy.wait(2000);
+}
+function verifySortCRAsc(){
+    var arr = [];
+      cy.get("a.VehicleReportLink__condition-report-link")
+        .should("be.visible")
+        .each(($listing) => {
+          cy.get($listing)
+            .invoke("attr", "data-value-text")
+            .then((cr) => {
+              arr.push(cr);
+            });
+        })
+        .then((checkIncrease) => {
+          expect(isIncreasing(arr), arr).to.be.true;
+        });
+}
+function verifySortCRDesc(){
+    var arr = [];
+      cy.get("a.VehicleReportLink__condition-report-link")
+        .should("be.visible")
+        .each(($listing) => {
+          cy.get($listing)
+            .invoke("attr", "data-value-text")
+            .then((cr) => {
+              arr.push(cr);
+            });
+        })
+        .then((checkIncrease) => {
+          expect(isIncreasing(arr), arr).to.be.false;
+        });
+}
+function verifySortYearAsc(){
+    cy.get('[data-test-id="listings"]')
+        .find(
+          '[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]'
+        )
+        .then((check) => {
+          var arr = [];
+          cy.wrap(check)
+            .each((listItem) => {
+              cy.wrap(listItem)
+                .find('[data-test-id="year"]')
+                .invoke("text")
+                .then((year) => {
+                  arr.push(Number(year));
+                });
+            })
+            .then((checkIncrease) => {
+              expect(isIncreasing(arr)).to.be.true;
+            });
+        });
+}
+function verifySortYearDesc(){
+    cy.get('[data-test-id="listings"]')
+        .find(
+          '[class="SearchResultsDetailView__container sw_row NoInfiniteScrollerStyles"]'
+        )
+        .then((check) => {
+          var arr = [];
+          cy.wrap(check)
+            .each((listItem) => {
+              cy.wrap(listItem)
+                .find('[data-test-id="year"]')
+                .invoke("text")
+                .then((year) => {
+                  arr.push(Number(year));
+                });
+            })
+            .then((checkIncrease) => {
+              expect(isIncreasing(arr)).to.be.false;
+            });
+        });
+}
+export const auctionPageSort = new PublicAuctionPageSort();
 var isIncreasing = (arr) => {
-    return (
-      arr.every((v, i) => i === 0 || v >= arr[i - 1])
-    );
-  };
+  return arr.every((v, i) => i === 0 || v >= arr[i - 1]);
+};
